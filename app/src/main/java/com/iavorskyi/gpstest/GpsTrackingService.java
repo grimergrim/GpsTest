09a620cbfa8158d2c3127f64a71d68a0bb3d17ca
Iavorskyi_Volodymyr <iavorskyi.v.v@gmail.com>
2017-04-07 13:48:16 +0300

    Initial commit

---
+++ app/src/main/java/com/iavorskyi/gpstest/GpsTrackingService.java
@@ -0,0 +1,76 @@
+package com.iavorskyi.gpstest;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+public class GpsTrackingService extends Service {
+
+    private static final int ONGOING_NOTIFICATION_ID = 15111984;
+
+    private Thread mThread;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.e("== tracking service ==", "onCreate");
+        mThread = new Thread("GPS tracking mThread") {
+            @Override
+            public void run() {
+                trackGps();
+            }
+        };
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (mThread != null && !mThread.isAlive()) {
+            mThread.start();
+            startForeground(ONGOING_NOTIFICATION_ID, buildNotification());
+        }
+        return START_REDELIVER_INTENT;
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.e("== tracking service ==", "onDestroy");
+        if (mThread != null && !mThread.isInterrupted()) {
+            mThread.interrupt();
+        }
+        super.onDestroy();
+    }
+
+    private void trackGps() {
+        while (true) {
+            Log.e("== tracking service ==", "Running. Time: " + System.currentTimeMillis());
+            try {
+                Thread.sleep(3 * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private Notification buildNotification() {
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+        return new Notification.Builder(this)
+                .setContentTitle(getText(R.string.notification_title))
+                .setContentText(getText(R.string.notification_message))
+                .setSmallIcon(R.drawable.ic_place_black_24dp)
+                .setContentIntent(pendingIntent)
+                .setTicker(getText(R.string.ticker_text))
+                .build();
+    }
+
+}
---
+++ app/src/main/java/com/iavorskyi/gpstest/MainActivity.java
@@ -0,0 +1,21 @@
+package com.iavorskyi.gpstest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Intent intent = new Intent(this, GpsTrackingService.class);
+        startService(intent);
+        finish();
+    }
+
+}
