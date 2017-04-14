4aca6ba21e21784948aee3556135c5035f8a3cbb
Iavorskyi_Volodymyr <iavorskyi.v.v@gmail.com>
2017-04-14 14:28:22 +0300

    Add MyApplication

---
+++ app/src/main/java/com/iavorskyi/gpstest/MyApplication.java
@@ -0,0 +1,11 @@
+package com.iavorskyi.gpstest;
+
+import android.support.multidex.MultiDexApplication;
+
+public class MyApplication extends MultiDexApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
